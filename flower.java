838a594328ecd98b8940ba7d7ecb5f4ed06f7b30
unknown <harshithxworkz@gmail.com>
2021-12-20 22:46:29 +0530

    example of learning 7

---
+++ flower.java
@@ -0,0 +1,9 @@
+class flower {
+     static String name= "raj";
+    static String [] names={" harshith","shree ram "," rayal","legend"};
+    public static void main (String  []args){
+    System.out.println(names[3]);
+    System.out.println(names[0]);
+    System.out.println(name);  
+   }
+}  
